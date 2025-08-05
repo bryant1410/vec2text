21f6193b0ed2cff601b72c79525f0b05b6e1e2a0
Santiago Castro <santiagoc@netflix.com>
2025-08-05 13:29:26 -0700

    Add a script to test CLIP inversion

---
+++ test_clip.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import transformers
+
+import vec2text
+
+
+def main() -> None:
+    last_checkpoint = transformers.trainer_utils.get_last_checkpoint(
+        "saves/openai-clip-vit-b-32-1"
+    )
+    print(last_checkpoint)
+
+    inversion_model = vec2text.models.InversionModel.from_pretrained(last_checkpoint)
+
+    # This is incorrect, but it doesn't matter for the test:
+    corrector_model = vec2text.models.CorrectorEncoderModel.from_pretrained(
+        last_checkpoint
+    )
+
+    corrector = vec2text.load_corrector(inversion_model, corrector_model)
+
+    print(
+        vec2text.invert_strings(
+            [
+                "A photo of a cat",
+                "A dog jumping over a fence",
+                "A video frame of an octopus playing the bass.",
+                "A photo of a steak",
+                "choripan",
+            ],
+            corrector=corrector,
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
